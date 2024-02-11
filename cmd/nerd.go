56d29310e02e5c201fa7a4ed886d9faed4126a6f
lacosteque <94007739+lacosteque@users.noreply.github.com>
2024-02-11 21:26:51 +0300

    Update nerd.go

--- cmd/nerd.go
+++ cmd/nerd.go
@@ -1,5 +1,28 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("hello nerd")
+    fmt.Println(`
+NAME:
+   nerd - versatile tool to support smart deployment process
+
+USAGE:
+   nerd [global options] command [command options] [arguments...]
+
+COMMANDS:
+   apply, a
+   login, l
+   config, c
+   edit, e
+   down
+   reload, r
+   up
+   update
+   distpacker
+   push
+   ps          List containers in all found compositions
+   help, h     Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --help, -h  show help (default: false)""")
+`)
 }
